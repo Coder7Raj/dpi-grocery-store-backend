10f0f62ac36e3e161a35b6db4a41c801e31d3957
mr9 asif <asifalibd002@gmail.com>
2025-06-24 18:07:18 +0600

    update usermodule

--- models/user.module.js
+++ models/user.module.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true, lowercase: true },
    password: { type: String, required: true,select: false },
   isAdmin: { type: Boolean, default: false },
-  image: { type: String, default: "" },
+
   isVerified: { type: Boolean, default: false },
   profileImage: { type: String, default: "" },
   payId: { type: String, unique: true },
